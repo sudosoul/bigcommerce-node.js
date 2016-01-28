d7fa95e60f2703b01470022182a81e5574a5c788
Robert Mullins <mullinsr@live.com>
2016-01-27 22:35:39 -0500

    Update products.js
    Convert indentation to spaces

--- examples/products.js
+++ examples/products.js
@@ -1,10 +1,10 @@
 var connection = require('./connection');
 
 var settings = {
-	path: 'https://store-hn1pzz9o.mybigcommerce.com',
-	user: 'surerob',
-	pass: 'redacted',
-	dataType: 'json'
+  path: 'https://store-hn1pzz9o.mybigcommerce.com',
+  user: 'surerob',
+  pass: 'redacted',
+  dataType: 'json'
 };
 
 var api = new connection(settings);
@@ -13,39 +13,39 @@ var api = new connection(settings);
 //GET Requests:
 //** Get first 50 Products and print their names **//
 api.get('/products')
-	.then(function(res) {
-		for (i=0; i<res.body.length; i++) {
-			console.log(res.body[i].name);
-		}
-	}).catch(function(err){
-		console.log(err);
-	});
+  .then(function(res) {
+    for (i=0; i<res.body.length; i++) {
+      console.log(res.body[i].name);
+    }
+  }).catch(function(err){
+    console.log(err);
+  });
 
 //** Power Of Promises, Nested Requests **//
-api.get('/products?limit=1')							    //Request single product from BC API.
-	.then(function(res) {
-		var categories = res.body[0].categories;	//Parse the categories array from the response.
-		categories.forEach(function(id) {         //ForEach ID in the categories array, 
-			api.get('/categories/' +id)             //Make a new request for that individual category,
-				.then(function(res) {
-					console.log(res.body);              //And print the API response for that category data. 
-				}).catch(function(err) {
-					console.log(err);
-				})
-		});
-	}).catch(function(err) {
-		console.log(err);
-	});
+api.get('/products?limit=1')                  //Request single product from BC API.
+  .then(function(res) {
+    var categories = res.body[0].categories;  //Parse the categories array from the response.
+    categories.forEach(function(id) {         //ForEach ID in the categories array, 
+      api.get('/categories/' +id)             //Make a new request for that individual category,
+        .then(function(res) {
+          console.log(res.body);              //And print the API response for that category data. 
+        }).catch(function(err) {
+          console.log(err);
+        })
+    });
+  }).catch(function(err) {
+    console.log(err);
+  });
 
 
 //** Update A Product **//
 var update = {inventory_level: 100};
 api.put('/products/1999', update)
-	.then(function(res){
-		console.log(res.body);
-	}).catch(function(err) {
-		console.log(err);
-	});
+  .then(function(res){
+    console.log(res.body);
+  }).catch(function(err) {
+    console.log(err);
+  });
 
 
 //** Update Product, Using XML **//
@@ -54,22 +54,13 @@ api.put('/products/1999', update)
 
 var update = {
    product: {
-		inventory_level: 900
-	}
+    inventory_level: 900
+  }
 };
 
 api.put('/products/1999', update)
-	.then(function(res){
-		console.log(res.body);
-	}).catch(function(err) {
-		console.log(err);
-	});
-
-
-
-
-
-
-
-
-
+  .then(function(res){
+    console.log(res.body);
+  }).catch(function(err) {
+    console.log(err);
+  });
